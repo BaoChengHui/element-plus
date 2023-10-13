e8a98482863d238f84952cd006e40f4f440725aa
baochenghui <e-Chenghui.Bao@geely.com>
2023-10-13 16:08:46 +0800

    update useSelect

--- packages/components/src/componsables/useSelect.ts
+++ packages/components/src/componsables/useSelect.ts
@@ -1,11 +1,11 @@
-import { ref, type MaybeRefOrGetter, computed } from "vue";
+import { ref, type MaybeRefOrGetter, computed, shallowRef } from "vue";
 import type { OpSelectProps } from "../select";
 import { useAsyncState } from "@vueuse/core";
 import type { Option } from "element-plus/es/components/select-v2/src/select.types";
 
-export interface UseSelectOptions {
+export interface UseSelectOptions<Params,Data> {
   props?: MaybeRefOrGetter<OpSelectProps>;
-  fetch?: () => Promise<any[]>;
+  fetch?:  (params:Params) => Promise<Data[]>
   /**
    * @default label
    */
@@ -16,33 +16,53 @@ export interface UseSelectOptions {
   valueKey?: string;
   formatter?: (data: any[]) => Option[];
   remote?: boolean;
-  data?:any[]
+  remoteKeywordKey?:string
+  data?: any[];
 }
 
-export interface UseSelectReturn {
+export interface UseSelectReturn {}
 
-}
+export function useSelect<Data, Params extends any[] = []>(
+  options: UseSelectOptions<Params,Data>
+) {
+  const { props = {}, fetch } = options;
+  const list = ref<Option[]>([]);
+  const remoteData = ref<Data[]>([])
 
-export function useSelect(options: UseSelectOptions) {
-  const { props = {}, fetch} = options;
-  const list = ref<Option[]>([])
-  
-  const fetchData = ()=>{
-    if(!fetch){
-        return 
+
+  const loading = ref(false)
+  const fetchData = async(params:Params) => {
+    if (!fetch) {
+      return;
     }
-  }
+    try {
+      const res = await fetch(params)
+      remoteData.value = res as any
+    } catch (error) {
+      
+    } 
+    
+    // const {isLoading} = useAsyncState();
+  };
 
-
-  const selectProps = computed<OpSelectProps>(()=>{
+  const selectProps = computed<OpSelectProps>(() => {
     return {
-        options:list.value
-    }
-  })
+      options: list.value,
+    };
+  });
 
   return {
     list,
     selectProps,
-    fetchData
-  }
+    fetchData,
+    remoteData
+  };
 }
+
+const fetchList = async():Promise<{name:string,value?:string}[]>=>{
+  return [{name:"111",value:"222"},{name:"111"}]
+}
+
+const {remoteData} = useSelect({
+  fetch:fetchList
+})
